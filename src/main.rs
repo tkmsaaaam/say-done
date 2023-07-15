26d8b641709d3e4602e22b5aab5b5a9627882048
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-07-15 20:08:28 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("monitoring {}", args[1]);
     for _i in 0..8640 {
-        let mut i = 0;
+        let mut target_process_count = 0;
         let output = Command::new("ps").output().expect("failed");
-        for line in String::from_utf8_lossy(&output.stdout).lines() {
-            if line.contains(&args[1]) {
-                i += 1;
+        for process in String::from_utf8_lossy(&output.stdout).lines() {
+            let process_splited: Vec<&str> = process.split_whitespace().collect();
+            for i in 3..process_splited.len() {
+                if process_splited[i].contains(&args[1]) {
+                    target_process_count += 1;
+                }
             }
         }
-        if i < 2 {
+        if target_process_count < 2 {
             Command::new("say").arg("Done!").output().expect("failed");
             break;
         } else {
