30499133d21b11809c563c90bd62dda1bbf470a8
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-07-18 06:21:14 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-extern crate libc;
 use std::env;
 use std::process::Command;
 use std::{thread, time};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("CMD is not defined.\nrun: cargo run CMD");
+        std::process::exit(0);
+    }
     println!("monitoring {}", args[1]);
     const INTERVAL: u64 = 10;
     const MAX_MONITERING_TIME: u64 = 60 * 60 * 24;
-    let self_pid = unsafe { libc::getpid() };
+    let self_pid = std::process::id();
 
-    for _i in 0..MAX_MONITERING_TIME / INTERVAL {
+    for i in 0..MAX_MONITERING_TIME / INTERVAL {
         let mut target_process_is_existed = false;
         let output = Command::new("ps").output().expect("failed");
         for process in String::from_utf8_lossy(&output.stdout).lines() {
             let process_splited: Vec<&str> = process.split_whitespace().collect();
-            if process_splited[0].eq(&self_pid.to_string()) {
+            if process_splited[0].eq("PID") || process_splited[0].eq(&self_pid.to_string()) {
                 continue;
             }
             if process_splited[3].starts_with(&args[1]) {
@@ -24,7 +27,23 @@ fn main() {
             }
         }
         if !target_process_is_existed {
+            if i == 0 {
+                println!("{} is not found. or {} is not started.", args[1], args[1]);
+                std::process::exit(0);
+            }
             Command::new("say").arg("Done!").output().expect("failed");
+            if env::consts::OS == "macos" {
+                let arg = String::from("display notification \"")
+                    + &args[1]
+                    + " was ended.\" with title \""
+                    + &args[1]
+                    + "\""; // display notification "CMD was ended." with title "CMD"
+                Command::new("osascript")
+                    .arg("-e")
+                    .arg(arg)
+                    .output()
+                    .expect("failed");
+            }
             break;
         } else {
             thread::sleep(time::Duration::from_secs(INTERVAL));
