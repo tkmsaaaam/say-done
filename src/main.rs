695d6f20f3fcd2b27ed394a9cdfa7d2797ff428d
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2023-07-25 06:37:40 +0900

    add tty to arg

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@ use std::{thread, time};
 
 #[derive(Parser)]
 struct Args {
-    #[arg(short = 't', long = "target")]
-    target: String,
+    #[arg(short = 'c', long = "command")]
+    command: String,
     #[arg(short = 'p', long = "pid")]
     pid: Option<String>,
+    #[arg(short = 't', long = "tty")]
+    tty: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
-    println!("monitoring {}", args.target);
+    println!("monitoring {}", args.command);
     const INTERVAL: u64 = 10;
     const MAX_MONITERING_TIME: u64 = 60 * 60 * 24;
     let self_pid = std::process::id();
@@ -26,25 +28,40 @@ fn main() {
             if process_splited[0].eq("PID") || process_splited[0].eq(&self_pid.to_string()) {
                 continue;
             }
-            if process_splited[3].starts_with(&args.target) {
+            if process_splited[3].starts_with(&args.command) {
+                let mut is_pid_present: bool = false;
                 match args.pid {
                     Some(ref pid) => {
                         if process_splited[0].eq(pid) {
-                            target_process_is_existed = true;
-                            break;
+                            is_pid_present = true;
                         }
                     }
                     None => {
-                        target_process_is_existed = true;
-                        break;
+                        is_pid_present = true;
                     }
+                }
+
+                let mut is_tty_present: bool = false;
+                match args.tty {
+                    Some(ref tty) => {
+                        if process_splited[1].eq(tty) {
+                            is_tty_present = true;
+                        }
+                    }
+                    None => {
+                        is_tty_present = true;
+                    }
+                }
+                if is_pid_present && is_tty_present {
+                    target_process_is_existed = true;
+                    break;
                 }
             }
         }
         if !target_process_is_existed && i == 0 {
             println!(
                 "{} is not found. or {} is not started.\nps result:",
-                args.target, args.target
+                args.command, args.command
             );
             println!("{}", String::from_utf8_lossy(&output.stdout));
             std::process::exit(0);
@@ -54,7 +71,7 @@ fn main() {
             println!("time: {}s", i * INTERVAL);
             if env::consts::OS == "macos" {
                 let arg = String::from("display notification \"")
-                    + &args.target
+                    + &args.command
                     + " was ended.\" with title \""
                     + env!("CARGO_PKG_NAME")
                     + "\""; // display notification "CMD was ended." with title "CMD"
@@ -68,5 +85,5 @@ fn main() {
         }
         thread::sleep(time::Duration::from_secs(INTERVAL));
     }
-    println!("{} has been running over an hour.", &args.target);
+    println!("{} has been running over an hour.", &args.command);
 }
